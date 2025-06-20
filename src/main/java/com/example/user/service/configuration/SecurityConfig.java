0a3ab6c1976a8eafb6ff6e44d6f29ae89b58792d
Bekzat <your@email.com>
2025-06-20 21:18:49 +0600

    Implement create user

--- src/main/java/com/example/user/service/configuration/SecurityConfig.java
+++ src/main/java/com/example/user/service/configuration/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,6 +21,11 @@ public class SecurityConfig {
                 )
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
---
+++ src/main/java/com/example/user/service/service/UserService.java
@@ -0,0 +1,29 @@
+package com.example.user.service.service;
+
+import com.example.user.service.dal.entity.UserEntity;
+import com.example.user.service.dal.repository.UserRepository;
+import com.example.user.service.dto.CreateUserDto;
+import com.example.user.service.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+    private final UserMapper mapper;
+    private final PasswordEncoder encoder;
+
+    @Transactional
+    public UUID create(CreateUserDto dto) {
+        UserEntity entity = mapper.map(dto);
+        entity.setPassword(encoder.encode(dto.getPassword()));
+        return repository.save(entity).getId();
+    }
+
+}
